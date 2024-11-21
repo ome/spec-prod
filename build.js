8d826a2fa280920eef80ea0def5e2aee967e5ca2
Josh Moore <josh@openmicroscopy.org>
2024-11-21 13:16:41 +0100

    Re-drop -f

--- build.js
+++ build.js
@@ -6,11 +6,11 @@ const source = env("INPUTS_SOURCE");
 
 (async () => {
 	console.log(`Converting Bikeshed document '${source}' to HTML...`);
-	await sh(`bikeshed -f spec "latest/${source}" "latest/index.out.html"`, "stream");
-	await sh(`bikeshed -f spec "0.5/${source}" "0.4/index.out.html"`, "stream");
-	await sh(`bikeshed -f spec "0.4/${source}" "0.4/index.out.html"`, "stream");
-	await sh(`bikeshed -f spec "0.3/${source}" "0.3/index.out.html"`, "stream");
-	await sh(`bikeshed -f spec "0.2/${source}" "0.2/index.out.html"`, "stream");
-	await sh(`bikeshed -f spec "0.1/${source}" "0.1/index.out.html"`, "stream");
+	await sh(`bikeshed spec "latest/${source}" "latest/index.out.html"`, "stream");
+	await sh(`bikeshed spec "0.5/${source}" "0.4/index.out.html"`, "stream");
+	await sh(`bikeshed spec "0.4/${source}" "0.4/index.out.html"`, "stream");
+	await sh(`bikeshed spec "0.3/${source}" "0.3/index.out.html"`, "stream");
+	await sh(`bikeshed spec "0.2/${source}" "0.2/index.out.html"`, "stream");
+	await sh(`bikeshed spec "0.1/${source}" "0.1/index.out.html"`, "stream");
 })()
 	.catch(err => exit(err.message || "Failed", err.code));
